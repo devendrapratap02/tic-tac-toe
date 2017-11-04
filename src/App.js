28e2ade92da98502bc4e4a892a5c0795f87a201e
Devendra Pratap Singh <dps.manit@gmail.com>
2017-11-04 12:06:43 +0530

    all done

--- src/App.js
+++ src/App.js
@@ -8,7 +8,8 @@ class App extends Component {
 		super(props);
 
 		this.handleClick = this.handleClick.bind(this);
-		this.checkGameStatus = this.checkGameStatus.bind(this);
+		this.indexToCoordinates = this.indexToCoordinates.bind(this);
+		this.handleReset = this.handleReset.bind(this);
 
 		this.magicSquare = [
 			[8, 1, 6],
@@ -16,13 +17,14 @@ class App extends Component {
 			[4, 9, 2]
 		];
 
-		this.player1 = [
-			[], [], []
-		];
+		this.player1 = Array(8).fill(0);
+		this.player2 = Array(8).fill(0);
 
-		this.player2 = [
-			[], [], []
-		];
+		this.winMatch = [
+			[[0, 3, 6], [0, 4], [0, 5, 7]],
+			[[1, 3], [1, 4, 6, 7], [1, 5]],
+			[[2, 3, 7], [2, 4], [2, 5, 6]]
+		]
 	}
 
 	componentWillMount() {
@@ -33,38 +35,47 @@ class App extends Component {
 
 	handleClick(index) {
 
-		let { cells } = this.state;
-		if(cells[index] !== ' ') return;
 
-		let count = cells.reduce((count, cell) => { if(cell != ' ') return count+1; else return count; }, 0)
-		if(count%2) {
-			p1.push(index);
-			cells[index] = 'X';
-			player = p1;
-		} else {
-			p2.push(index);
-			cells[index] = 'O';
-			player = p2;
+		let { cells } = this.state;
+
+		const count = cells.reduce((count, cell) => { return (cell !== ' ') ? count+1 : count }, 0);
+		const symbol = (count%2) ? 'O' : 'X';
+		cells[index] = symbol;
+		let { tx, ty } = this.indexToCoordinates(index);
+		let player = (count%2) ? this.player2 : this.player1;
+		const sval = this.magicSquare[tx][ty];
+		let winStatus = false;
+
+		for(let pIndex of this.winMatch[tx][ty]) {
+			player[pIndex] += sval;
+			if(player[pIndex] === 15) {
+				winStatus = true;
+				break;
+			}
 		}
 
-		
-		if(count < 5) {
-			this.setState({
-				cells, p1, p2
-			});
-			return;
-		};
-
-		this.checkGameStatus({cells, p1, p2, player: player.sort()});
+		this.setState({
+			cells, symbol, winStatus
+		});
 	}
 
-	checkGameStatus(newState) {
-		this.setState(newState);
+	indexToCoordinates(index) {
+		return { tx: parseInt(index/3, 10), ty: index%3 }
+	}
+
+	handleReset() {
+		this.player1 = Array(8).fill(0);
+		this.player2 = Array(8).fill(0);
+
+		this.setState({
+			cells: Array(9).fill(' '),
+			winStatus: false
+		});
 	}
 
 	render() {
 		const cells = this.state.cells.map((cell, index) => {
-			return <div className={ "cell " + (cell === ' ' ? 'cursor-enable' : 'cursor-disable') } key={`cell_${index}`} onClick={() => { this.handleClick(index); }}><span>{cell}</span></div>
+			return <div className={ "cell " + (cell === ' ' ? 'cursor-enable' : 'cursor-disable') } key={`cell_${index}`} onClick={() => { if(cell !== ' ') return; this.handleClick(index); }}><span>{cell}</span></div>
 		});
 		return (
 		<div className="App">
@@ -73,10 +84,16 @@ class App extends Component {
 				<h1 className="App-title">Welcome to Tic-Tac-Toe</h1>
 			</header>
 			<div className="App-intro">
+				<div className={ this.state.winStatus ? "win-row" : "win-row hide" }>
+					<p>Winner is Player - {this.state.symbol}</p>
+				</div>
 				<div className="square-box">
 					<div className="square-content">
 						{cells}
 					</div>
+				</div>
+				<div className="btn-toolbar">
+					<button type="reset" className="btn" onClick={this.handleReset}>Reset</button>
 				</div>
 			</div>
 		</div>
